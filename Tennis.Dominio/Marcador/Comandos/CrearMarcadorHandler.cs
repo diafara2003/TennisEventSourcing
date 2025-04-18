21f6a910629974c5d5630a302f16fe5a06ad3385
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2025-04-17 19:22:14 -0500

    mensaje handle

--- Tennis.Dominio/Marcador/Comandos/CrearMarcadorHandler.cs
+++ Tennis.Dominio/Marcador/Comandos/CrearMarcadorHandler.cs
@@ -3,6 +3,7 @@
 public record CrearMarcador(Guid IdMarcador,string puntaje);
 public class CrearMarcadorHandler(IEventStore eventStore) : ICommandHandler<CrearMarcador>
 {
+    //El método debe llamarse Handle o HandleAsync, debe ser public, y debe recibir como primer parámetro el tipo de mensaje.
     public Task Handle(CrearMarcador command)
     {
         var marcadorCreado = new Eventos.MarcadorCreado(command.IdMarcador,command.puntaje);
